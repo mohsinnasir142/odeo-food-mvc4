2639afd294648949c13657c29250b5af00febd7b
mohsinnasir142 <mohsinnasir142@gmail.com>
2014-02-26 09:41:09 +0000

    total project

---
+++  odeo-food-mvc4/OdeoFoodMVC4/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OdeoFoodMVC4
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Cusine","cusine/{name}", //pattern  of URL
+                new { controller = "Cusine", action = "Search", name = UrlParameter.Optional } //Controller information Default name is empty
+                         );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++  odeo-food-mvc4/OdeoFoodMVC4/Controllers/CusineController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeoFoodMVC4.Controllers
+{
+    public class CusineController : Controller
+    {
+        //
+        // GET: /Cusine/
+
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+            return Content("Hello ! your selected cusine is  "+message + " and Orignal name is "+name);
+        }
+
+    }
+}
