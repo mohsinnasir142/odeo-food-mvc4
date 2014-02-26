eb78357e6588f75caceed8f97ff9ff5494ecc238
mohsinnasir142@gmail.com <mohsinnasir142@gmail.com@809f08fe-e71c-8910-6250-65888085b232>
2014-02-26 09:41:09 +0000

    total project

---
+++ trunk/ odeo-food-mvc4/OdeoFoodMVC4/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeoFoodMVC4.Models;
+
+namespace OdeoFoodMVC4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            var ModelHome = new homeModel();
+
+            ModelHome.getAddress = "Township";
+            ModelHome.getLocation = "Pakistan   "+DateTime.UtcNow;
+            return View( ModelHome);
+        }
+
+        public ActionResult About()
+        {   
+           // AboutModel models=new AboutModel();
+            var models = new AboutModel();
+            //both lines will work
+            models.Name = "Mohsin";
+            models.Location = "Lahore";
+            return View(models);
+        }
+
+        public ActionResult Contact()
+        {
+            var controller = RouteData.Values["controller"].ToString()+" / " + RouteData.Values["action"].ToString()+" / " + RouteData.Values["id"];
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = " this is router "+controller;
+
+            return View();
+        }
+    }
+}
---
+++ trunk/ odeo-food-mvc4/OdeoFoodMVC4/Models/homeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeoFoodMVC4.Models
+{
+    public class homeModel
+    {
+        public string getAddress { get; set; }
+        public string getLocation { get; set; }
+    }
+}
