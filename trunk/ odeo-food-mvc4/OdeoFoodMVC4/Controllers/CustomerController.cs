e8c838d22e75103f7c3c64af1101c7137f474718
mohsinnasir142@gmail.com <mohsinnasir142@gmail.com@809f08fe-e71c-8910-6250-65888085b232>
2014-03-14 10:42:25 +0000

    Roll based access continue

---
+++ trunk/ odeo-food-mvc4/OdeoFoodMVC4/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeoFoodMVC4.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllCustomer()
+        {
+            return View();
+        }
+        public ActionResult Sales()
+        {
+            return View();
+        }
+
+        public ActionResult CreateOrder()
+        {
+            return View();
+        }
+    }
+}
